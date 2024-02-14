7df8ae1e4adea53e5a07ca9f772ae5f722ab710a
sevgi75 <erbilsevgi97@gmail.com>
2024-02-14 17:23:15 +0300

    feat: add NavBar

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,21 +1,32 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-import PrivateRouter from "./PrivateRouter";
+// import PrivateRouter from "./PrivateRouter";
 import Dashboard from "../pages/Dashboard";
+import NotFound from "../pages/NotFound";
+import NavBar from "../components/NavBar";
+import FooTer from "../components/FooTer";
+import NewBlog from "../pages/NewBlog";
+import About from "../pages/About";
 
 const AppRouter = () => {
   return (
-    <Router>
+    <BrowserRouter>
+    <NavBar />
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Dashboard/>}/>
+        <Route path="/newBlog" element={<NewBlog />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/login" element={<Login />} />
         <Route path="register" element={<Register />} />
-        <Route path="stock" element={<PrivateRouter />}>
+        {/* <Route path="stock" element={<PrivateRouter />}>
           <Route path="" element={<Dashboard />} />
-        </Route>
+        </Route> */}
+        <Route path="*" element={<NotFound />}/>
       </Routes>
-    </Router>
+      <FooTer />
+    </BrowserRouter>
   );
 };
 
