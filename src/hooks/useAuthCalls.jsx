bd7982c4a03f7eea1a1db43d3112a9b8b58ca0d8
sevgi75 <erbilsevgi97@gmail.com>
2024-02-17 16:57:31 +0300

    feat:add useAuthCalls login

--- src/hooks/useAuthCalls.jsx
+++ src/hooks/useAuthCalls.jsx
@@ -1,8 +1,27 @@
+import { useDispatch } from "react-redux"
+import { fetchFail, fetchStart, loginSuccess } from "../features/authSlice"
+import { useNavigate } from "react-router-dom"
+import useAxios from "./useAxios"
+import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify"
 
 const useAuthCalls = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const {axiosWithToken, axiosPublic} = useAxios()
 
   const login = async(userInfo)=>{
-    
+    dispatch(fetchStart())
+    try {
+      const {data} = await axiosPublic.post("auth/login/", userInfo)
+      console.log(data);
+      toastSuccessNotify("Login işlemi başarili.")
+      dispatch(loginSuccess(data))
+      navigate("/")
+    } catch (error) {
+      console.log(error);
+      toastErrorNotify("Login işlemi başarisiz!")
+      dispatch(fetchFail())      
+    }
   }
   return {login}
 }
