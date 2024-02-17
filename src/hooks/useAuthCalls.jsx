af9a74f17c4a193d075dd6764565f382fde0d2e9
sevgi75 <erbilsevgi97@gmail.com>
2024-02-17 17:14:50 +0300

    feat:add useAuthCalls logout

--- src/hooks/useAuthCalls.jsx
+++ src/hooks/useAuthCalls.jsx
@@ -1,5 +1,5 @@
 import { useDispatch } from "react-redux"
-import { fetchFail, fetchStart, loginSuccess, registerSuccess } from "../features/authSlice"
+import { fetchFail, fetchStart, loginSuccess, logoutSuccess, registerSuccess } from "../features/authSlice"
 import { useNavigate } from "react-router-dom"
 import useAxios from "./useAxios"
 import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify"
@@ -37,7 +37,21 @@ const useAuthCalls = () => {
       dispatch(fetchFail())
     }
   }
-  return {login}
+  const logout = async () => {
+    dispatch(fetchStart())
+    try {
+      const {data} = await axiosWithToken("/auth/logout/")
+      console.log(data);
+      toastSuccessNotify("Logout işlemi başarili.")
+      dispatch(logoutSuccess())
+      navigate("/")
+    } catch (error) {
+      console.log(error);
+      toastErrorNotify("Logout işlemi başarisiz oldu!")
+      dispatch(fetchFail())
+    }
+  }
+  return {login, register, logout}
 }
 
 export default useAuthCalls
